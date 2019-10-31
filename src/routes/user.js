18f1c5d465481bd0c46230a002a0ad7adb4d2dc5
Amjad Khan <amjadkhan@Amjads-MacBook-Pro.local>
2019-10-31 22:41:45 +0400

    some changes

--- src/routes/user.js
+++ src/routes/user.js
@@ -20,7 +20,8 @@ router.post('/register',async (req,res) => {
 
     const user = new User(req.body);
     try{
-        const token =  user.newAuthToken()
+        const token = await user.newAuthToken()
+        res.status(201).send({user, token})
     }catch(e){
         res.status(400).send(e)
     }
