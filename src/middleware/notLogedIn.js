05ae26cb105d477315cf0253a573bcb9cc76fd1f
Amjad Khan <amjadkhan@Amjads-MacBook-Pro.local>
2019-11-03 06:37:16 +0400

    register and login issue fix due to middleware

--- src/middleware/notLogedIn.js
+++ src/middleware/notLogedIn.js
@@ -3,7 +3,7 @@ const jwt  = require('jsonwebtoken')
 const notLogedIn = async (req,res,next) => {
 
         //console.log(req.cookies,'----');
-        const token =  req.cookies.token || req.header('Authorization').replace('Bearer', '').trim()
+        const token =  req.cookies.token
 
        if(token)
            res.redirect('/users/me')
